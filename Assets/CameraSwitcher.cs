734ffd1dd63f7554292b33bd17abad1a861e0105
Skyval <36752399+AzenForze@users.noreply.github.com>
2019-02-09 19:11:05 -0600

    Initial Commit

---
+++ Assets/CameraSwitcher.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public GameObject flyCamera;
+
+    UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController FPController;
+    Camera FPCamera;
+    Rigidbody body;
+
+    bool flying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FPController = GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController>();
+        FPCamera = FPController.cam;
+        body = GetComponent<Rigidbody>();
+
+        FPController.enabled = true;
+        FPCamera.enabled = true;
+        flyCamera.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("c"))
+        {
+            flying = !flying;
+
+            if(flying)
+            {
+                FPController.enabled = false;
+                FPCamera.enabled = false;
+                flyCamera.SetActive(true);
+                flyCamera.transform.position = FPController.transform.position;
+                flyCamera.transform.rotation = FPController.transform.rotation;
+            }
+            else
+            {
+                FPController.enabled = true;
+                FPCamera.enabled = true;
+                FPCamera.enabled = true;
+                flyCamera.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Freelook.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freelook : MonoBehaviour
+{
+    int baseSpeed = 10;
+
+    float rotationX = 0f;
+    float rotationY = 0f;
+
+    public int XSensitivity = 100;
+    public int YSensitivity = 100;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // instead of going from 0 to 360 cw, go from -180 to 180 ccw, keeping 0 the same
+        var direction = 540 - transform.localEulerAngles.x; // (360 - x) + 180 = 540 - x
+        direction %= 360;
+        direction -= 180;
+
+        rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * XSensitivity;
+        rotationX = ((rotationX+180) % 360)-180;
+        rotationY = direction + Input.GetAxis("Mouse Y") * YSensitivity;
+        rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+
+        
+
+        if (Input.GetKey("w")) transform.position += transform.forward * baseSpeed * Time.deltaTime;
+        else if (Input.GetKey("s")) transform.position -= transform.forward * baseSpeed * Time.deltaTime;
+
+        if (Input.GetKey("a")) transform.position += -transform.right * baseSpeed * Time.deltaTime;
+        else if (Input.GetKey("d")) transform.position += transform.right * baseSpeed * Time.deltaTime;
+    }
+}
