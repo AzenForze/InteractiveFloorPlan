268473060153349b484116b8cbb15e372d71b628
Skyval <36752399+AzenForze@users.noreply.github.com>
2019-02-15 22:37:54 -0600

    Added gridSnap
    Walls now snap to grid. It's not great, but it's better than nothing.

--- Assets/Scripts/CreateWalls.cs
+++ Assets/Scripts/CreateWalls.cs
@@ -42,7 +42,7 @@ public class CreateWalls : MonoBehaviour
     void setStart()
     {
         creating = true;
-        startPos = getWorldPoint();
+        startPos = gridSnap(getWorldPoint());
         wall = Instantiate(wallPrefab, startPos, Quaternion.identity);
     }
 
@@ -53,7 +53,7 @@ public class CreateWalls : MonoBehaviour
 
     void adjust()
     {
-        endPos = getWorldPoint();
+        endPos = gridSnap(getWorldPoint());
         adjustWall();
     }
 
@@ -62,7 +62,20 @@ public class CreateWalls : MonoBehaviour
         wall.transform.LookAt(endPos);
         var distance = Vector3.Distance(startPos, endPos);
         wall.transform.position = startPos + distance / 2 * (endPos - startPos).normalized;
-        wall.transform.localScale = new Vector3(wall.transform.localScale.x, wall.transform.localScale.y, distance);
+        wall.transform.localScale = new Vector3(wall.transform.localScale.x, wall.transform.localScale.y, distance + (float)0.5);
+    }
+
+    Vector3 gridSnap(Vector3 position, float granularity = (float)1.0)
+    {
+        float xpos = position.x;
+        xpos = xpos / granularity;
+        xpos = Mathf.Round(xpos) * granularity;
+
+        float zpos = position.z;
+        zpos = zpos / granularity;
+        zpos = Mathf.Round(zpos) * granularity;
+
+        return new Vector3(xpos, position.y, zpos);
     }
 
     Vector3 getWorldPoint()
